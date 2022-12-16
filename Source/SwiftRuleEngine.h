cfd42ec5cba5fdae72336a2fd9964803eeeba8a6
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2022-12-16 10:32:41 -0300

    Add base project

---
+++ Source/SwiftRuleEngine.h
@@ -0,0 +1,18 @@
+//
+//  SwiftRuleEngine.h
+//  SwiftRuleEngine
+//
+//  Created by Santiago Alvarez on 16/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftRuleEngine.
+FOUNDATION_EXPORT double SwiftRuleEngineVersionNumber;
+
+//! Project version string for SwiftRuleEngine.
+FOUNDATION_EXPORT const unsigned char SwiftRuleEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRuleEngine/PublicHeader.h>
+
+
