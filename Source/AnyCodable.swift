822e58605ee1479f22e752231f3f673bf97db9e0
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-02-18 23:05:20 -0300

    Add valueType enum to AnyCodable

--- Source/AnyCodable.swift
+++ Source/AnyCodable.swift
@@ -7,29 +7,52 @@
 
 import Foundation
 
-struct AnyCodable: Decodable {
-    private var value: Any
+enum Type: Int {
+    case number
+    case string
+    case bool
+    case array
+    case dictionary
+    case null
+    case unknown
+}
 
-    init<T>(_ value: T?) {
-        self.value = value ?? ()
-    }
+struct AnyCodable: Decodable {
+    var value: Any
+    var valueType: Type
 
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         if container.decodeNil() {
-            self.value = ()
-        } else if let bool = try? container.decode(Bool.self) {
-            self.value = bool
-        } else if let int = try? container.decode(Int.self) {
-            self.value = int
-        } else if let double = try? container.decode(Double.self) {
-            self.value = double
+            self.value = NSNull()
+            self.valueType = .null
+            
         } else if let string = try? container.decode(String.self) {
             self.value = string
+            self.valueType = .string
+                       
+        } else if let bool = try? container.decode(Bool.self) {
+            self.value = bool
+            self.valueType = .bool
+            
+        } else if let int = try? container.decode(Int.self) {
+            self.value = int
+            self.valueType = .number
+            
+        } else if let double = try? container.decode(Double.self) {
+            self.value = double
+            self.valueType = .number
+
         } else if let array = try? container.decode([AnyCodable].self) {
-            self.value = array.map { $0.value }
+            // TODO: watch out for this
+            self.value = array
+            self.valueType = .array
+            
         } else if let dictionary = try? container.decode([String: AnyCodable].self) {
-            self.value = dictionary.mapValues { $0.value }
+            // TODO: also watch out for this
+            self.value = dictionary
+            self.valueType = .dictionary
+
         } else {
             throw DecodingError.dataCorruptedError(in: container, debugDescription: "AnyCodable value cannot be decoded")
         }
