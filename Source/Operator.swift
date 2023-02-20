74e054d91b8a4e3d2b8aa86b4e5cd5d5a6df5e3d
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-02-20 14:35:51 -0300

    Add placeholders for default operators

--- Source/Operator.swift
+++ Source/Operator.swift
@@ -22,3 +22,75 @@ struct Equal: Operator {
         return true
     }
 }
+
+struct NotEqual: Operator {
+    var id = "not_equal"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct LessThan: Operator {
+    var id = "less_than"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct LessThanInclusive: Operator {
+    var id = "less_than_inclusive"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct GreaterThan: Operator {
+    var id = "greater_than"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct GreaterThanInclusive: Operator {
+    var id = "greater_than_inclusive"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct In: Operator {
+    var id = "in"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct NotIn: Operator {
+    var id = "not_in"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct Contains: Operator {
+    var id = "contains"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
+
+struct NotContains: Operator {
+    var id = "not_contains"
+    
+    func match(_ condition: SimpleCondition, _ objValue: Any) -> Bool {
+        return true
+    }
+}
