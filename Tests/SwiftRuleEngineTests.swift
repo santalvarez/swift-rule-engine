f903ab117d8f07bbfcffa1902ea358e999085cc7
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-02-20 21:40:58 -0300

    Add first test

--- Tests/SwiftRuleEngineTests.swift
+++ Tests/SwiftRuleEngineTests.swift
@@ -18,19 +18,41 @@ class SwiftRuleEngineTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
+    func testRuleEngine() throws {
+        let rule: [String: Any] = [
+            "name": "test-rule",
+            "description": "Test rule",
+            "conditions": [
+                "any": [
+                    [
+                        "path": "$.person.name",
+                        "value": "Santiago",
+                        "operator": "equal"
+                    ]
+                ]
+            ]
+        ]
+        
+        let obj = [
+            "person": [
+                "name": "Santiago"
+            ]
+        ]
+        
+        let engine = try RuleEngine(rules: [rule])
+
+        let r = engine.evaluate(obj)
+        let result = try XCTUnwrap(r)
+        
+        XCTAssertTrue(result.conditions.match)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
