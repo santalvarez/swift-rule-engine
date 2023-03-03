8ce06d148c5accc441f8a498cf46cd44f1dce25c
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-03-03 09:51:28 -0300

    Add Regex tests

--- Tests/OperatorTests/RegexOperatorTests.swift
+++ Tests/OperatorTests/RegexOperatorTests.swift
@@ -6,30 +6,138 @@
 //
 
 import XCTest
+@testable import SwiftRuleEngine
+
 
 class RegexOperatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMatchIPAddress() {
+        let lhs = AnyCodable(value: "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(.(?!$)|$)){4}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "192.168.0.1"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
+    }
+    
+    func testMatchInvalidDate() {
+        let lhs = AnyCodable(value: "^\\d{4}-\\d{2}-\\d{2}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "03/03/2023"
+        
+        let op = Regex()
+        
+        XCTAssertFalse(op.match(condition, rhs))
+    }
+    
+    func testMatchValidURL() {
+        let lhs = AnyCodable(value: "^https?://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "http://www.example.com/index.html"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMatchInvalidURL() {
+        let lhs = AnyCodable(value: "^https?://([\\w-]+\\.)+[\\w-]+(/[\\w-./?%&=]*)?$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "invalid-url"
+        
+        let op = Regex()
+        
+        XCTAssertFalse(op.match(condition, rhs))
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testMatchValidDate() {
+        let lhs = AnyCodable(value: "^\\d{4}-\\d{2}-\\d{2}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "2023-03-03"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMatchValidEmailAddress() {
+        let lhs = AnyCodable(value: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "test@example.com"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
     }
 
+    func testMatchInvalidEmailAddress() {
+        let lhs = AnyCodable(value: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "invalid-email-address"
+        
+        let op = Regex()
+        
+        XCTAssertFalse(op.match(condition, rhs))
+    }
+
+    func testMatchValidUSPhoneNumber() {
+        let lhs = AnyCodable(value: "^\\(\\d{3}\\) \\d{3}-\\d{4}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "(555) 555-5555"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
+    }
+
+    func testMatchInvalidUSPhoneNumber() {
+        let lhs = AnyCodable(value: "^\\(\\d{3}\\) \\d{3}-\\d{4}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "555-555-5555"
+        
+        let op = Regex()
+        
+        XCTAssertFalse(op.match(condition, rhs))
+    }
+
+    func testMatchValidCreditCardNumber() {
+        let lhs = AnyCodable(value: "^(\\d{4}[- ]?){3}\\d{4}|\\d{16}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "4111-1111-1111-1111"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
+    }
+
+    func testMatchInvalidCreditCardNumber() {
+        let lhs = AnyCodable(value: "^(\\d{4}[- ]?){3}\\d{4}|\\d{16}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "invalid-credit-card-number"
+        
+        let op = Regex()
+        
+        XCTAssertFalse(op.match(condition, rhs))
+    }
+
+    func testMatchValidPassword() {
+        let lhs = AnyCodable(value: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "Password123"
+        
+        let op = Regex()
+        
+        XCTAssertTrue(op.match(condition, rhs))
+    }
+
+    func testMatchInvalidPassword() {
+        let lhs = AnyCodable(value: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", valueType: .string)
+        let condition = SimpleCondition(op: "regex", value: lhs)
+        let rhs: Any = "password"
+        
+        let op = Regex()
+        
+        XCTAssertFalse(op.match(condition, rhs))
+    }
 }
