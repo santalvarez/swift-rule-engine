ee35ae7545e00251d66a086bb501a5508dbe4f23
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-03-27 00:30:41 -0300

    Refactor RuleEngine to use inout params

--- Source/RuleEngine.swift
+++ Source/RuleEngine.swift
@@ -18,7 +18,7 @@ final public class RuleEngine {
     private var operators: [OperatorID: Operator]
     private var rules: [Rule] = []
     private let pathParser: PathParser = PathParser()
-    
+
 
     // Init for rules that come in string format
     public init(rules: [String], customOperators: [Operator] = []) throws {
@@ -34,7 +34,12 @@ final public class RuleEngine {
             try? Self.decodeRule(rule: dictRule)
         }
     }
-    
+
+    public init(rules: [Rule], customOperators: [Operator] = []) throws {
+        self.rules = rules
+        self.operators = try Self.generateOperatorsDict(customOperators)
+    }
+
     private static func generateOperatorsDict(_ customOperators: [Operator]) throws -> [OperatorID:Operator]{
         let defaultOperators: [Operator] = [Equal(), NotEqual(), LessThan(), LessThanInclusive(),
                                             GreaterThan(), GreaterThanInclusive(), In(), NotIn(),
@@ -48,7 +53,7 @@ final public class RuleEngine {
             result[op.id] = op
         }
     }
-    
+
     private static func decodeRule(rule: [String: Any]) throws -> Rule {
         guard let ruleData = try? JSONSerialization.data(withJSONObject: rule, options: []) else {
             throw RuleEngineError.invalidRule("Error converting dict to data")
@@ -59,87 +64,97 @@ final public class RuleEngine {
         }
         return rule
     }
-    
+
     private static func decodeRule(rule: String) throws -> Rule {
         guard let ruleData = rule.data(using: .utf8) else {
             throw RuleEngineError.invalidRule("Rule not in utf8 format")
         }
-        
+
         guard let rule = try? JSONDecoder().decode(Rule.self, from: ruleData) else {
             throw RuleEngineError.invalidRule("Error decoding rule")
         }
         return rule
     }
 
-    private func runCondition(_ condition: SimpleCondition, _ obj: Any) throws -> SimpleCondition {
-        var condition = condition
+    private func runCondition(_ condition: inout SimpleCondition, _ obj: Any) throws {
         let pathObj: Any
         if condition.path != nil {
             pathObj = try self.pathParser.getValue(condition.path!, obj)
         } else {
             pathObj = obj
         }
-        
+
         guard let op = self.operators[condition.op] else {
             throw RuleEngineError.operatorNotFound
         }
-        
+
         condition.match = op.match(condition, pathObj)
-        
-        return condition
     }
 
-    private func runMultiCondition(_ multiCondition: MultiCondition, _ obj: Any) throws -> MultiCondition {
-        var multiCondition = multiCondition
-        
-        var conditions = multiCondition.getConditions()
-
-        var allMatch = true
-        
-        for (idx, condition) in conditions.enumerated() {
+    private func runMultiConditionAll(_ multiCondition: inout MultiCondition, _ obj: Any) throws {
+        for (idx, condition) in multiCondition.all!.enumerated() {
             let result: Condition
-            
+
             switch condition {
-            case .multi(let cond):
-                result = Condition(try self.runMultiCondition(cond, obj))
+            case .multi(var cond):
+                try self.runMultiCondition(&cond, obj)
+                result = Condition.multi(cond)
 
-            case .simple(let cond):
-                result = Condition(try self.runCondition(cond, obj))
+            case .simple(var cond):
+                try self.runCondition(&cond, obj)
+                result = Condition.simple(cond)
             }
-            
-            conditions[idx] = result
-            
-            if multiCondition.any != nil {
-                if result.getMatch() {
-                    multiCondition.match = true
-                    break
-                }
-            } else {
-                if !result.getMatch() {
-                    allMatch = false
-                    break
-                }
+
+            multiCondition.all![idx] = result
+
+            if !result.getMatch() {
+                multiCondition.match = false
+                return
             }
         }
-        
+
+        multiCondition.match = true
+    }
+
+    private func runMultiConditionAny(_ multiCondition: inout MultiCondition, _ obj: Any) throws {
+        for (idx, condition) in multiCondition.any!.enumerated() {
+            let result: Condition
+
+            switch condition {
+            case .multi(var cond):
+                result = Condition(try self.runMultiCondition(&cond, obj))
+
+            case .simple(var cond):
+                result = Condition(try self.runCondition(&cond, obj))
+            }
+
+            multiCondition.any![idx] = result
+
+            if result.getMatch() {
+                multiCondition.match = true
+                return
+            }
+        }
+
+        multiCondition.match = false
+    }
+
+    private func runMultiCondition(_ multiCondition: inout MultiCondition, _ obj: Any) throws {
         if multiCondition.all != nil {
-            multiCondition.match = allMatch
+            try self.runMultiConditionAll(&multiCondition, obj)
+        } else if multiCondition.any != nil {
+            try self.runMultiConditionAny(&multiCondition, obj)
         }
-
-        multiCondition.replaceConditions(conditions)
-        
-        return multiCondition
     }
 
     public func evaluate(_ obj: [String: Any]) -> Rule? {
         for rule in self.rules {
             var rule = rule
-            guard let result = try? self.runMultiCondition(rule.conditions, obj) else {
+            guard (try? self.runMultiCondition(&rule.conditions, obj)) != nil else {
                 continue
             }
 
-            if result.match {
-                rule.conditions = result
+            if rule.conditions.match {
                 return rule
             }
         }
