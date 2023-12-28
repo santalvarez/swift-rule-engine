948d08564a936bdbdf1e989a05df816b029e1a24
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2023-12-28 15:39:32 -0300

    Add array subscript JSONPath support

--- Source/JSONPath.swift
+++ Source/JSONPath.swift
@@ -13,39 +13,74 @@ enum JSONPathError: Error {
     case expectingDictionary
 }
 
+enum JSONPart {
+    case key(String)
+    case index(Int)
+}
+
 
 public struct JSONPath {
-   let components: [String]
+    private let parts: [JSONPart]
+    private let pathRegex = try! NSRegularExpression(pattern: #"\$\.((\w+\[\d+\](\.|$)|(\w+\.)))*(\w+\[\d+\]|\w+)$"#)
 
-   init(_ path: String) throws {
-       if !path.starts(with: "$.") {
-           throw JSONPathError.invalidPath
-       }
+    init(_ path: String) throws {
+        guard path != "$" else {
+            self.parts = []
+            return
+        }
 
-       self.components = path.dropFirst(2).components(separatedBy: ".")
-   }
+        guard pathRegex.firstMatch(in: path, options: [],
+                                   range: NSRange(location: 0, length: path.count)) != nil else {
+            throw JSONPathError.invalidPath
+        }
 
-   private func accessDict(_ key: String, _ dict: [String: Any]) throws -> Any {
-       if let value = dict[key] {
-           return value
-       } else if dict[key] == nil {
-           return NSNull()
-       } else {
-           throw JSONPathError.valueNotFound
-       }
-   }
+        self.parts = try path.components(separatedBy: ".").dropFirst().map { char in
+            if char.hasPrefix("[") && char.hasSuffix("]") {
+                guard let index = Int(char.dropFirst().dropLast()) else {
+                    throw JSONPathError.invalidPath
+                }
+                return .index(index)
+            } else {
+                return .key(char)
+            }
+        }
+    }
 
-   func getValue(for obj: Any) throws -> Any {
-       var currentObj: Any = obj
+    private func accessDict(_ key: String, _ dict: [String: Any]) throws -> Any {
+        if let value = dict[key] {
+            return value
+        } else if dict[key] == nil {
+            return NSNull()
+        } else {
+            throw JSONPathError.valueNotFound
+        }
+    }
 
-       for component in self.components {
-           guard let dict = currentObj as? [String: Any] else {
-               throw JSONPathError.expectingDictionary
-           }
-           currentObj = try self.accessDict(component, dict)
-       }
+    private func accessArray(_ index: Int, _ array: [Any]) throws -> Any {
+        if index < array.count {
+            return array[index]
+        } else {
+            throw JSONPathError.valueNotFound
+        }
+    }
 
-       return currentObj
+    func getValue(for obj: Any) throws -> Any {
+        var currentObj: Any = obj
 
-   }
+        for p in self.parts {
+            switch p {
+            case .key(let key):
+                guard let dict = currentObj as? [String: Any] else {
+                    throw JSONPathError.expectingDictionary
+                }
+                currentObj = try self.accessDict(key, dict)
+            case .index(let index):
+                guard let array = currentObj as? [Any] else {
+                    throw JSONPathError.expectingDictionary
+                }
+                currentObj = try self.accessArray(index, array)
+            }
+        }
+        return currentObj
+    }
 }
