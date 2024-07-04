3a4aedd51cb3644a79d284cd0cfeb739043c3179
Santiago Alvarez <santiago.salvarez@mercadolibre.com>
2024-07-04 11:23:09 -0300

    Add StringSubscriptableMacro

---
+++ Sources/SwiftRuleEngineMacros/StringSubscriptableMacro.swift
@@ -0,0 +1,97 @@
+//
+//  StringSubscriptableMacro.swift
+//
+//
+//  Created by Santiago Alvarez on 04/07/2024.
+//
+
+import SwiftCompilerPlugin
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+import Foundation
+
+
+enum StringSubscriptableMacroError: Error, CustomStringConvertible {
+    case invalidAppliedType
+    case withKeysInvalidType
+
+    var description: String {
+        switch self {
+        case .invalidAppliedType: return "@StringSubscriptable is only applicable to Struct or Class types"
+        case .withKeysInvalidType: return "withKeys attribute must be a boolean"
+        }
+    }
+}
+
+public struct StringSubscriptableMacro: ExtensionMacro {
+    public static func expansion(of node: SwiftSyntax.AttributeSyntax,
+                                 attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
+                                 providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
+                                 conformingTo protocols: [SwiftSyntax.TypeSyntax],
+                                 in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
+
+        var generateKeys = false
+        if let args = node.arguments?.as(LabeledExprListSyntax.self) {
+            for arg in args {
+                if arg.label?.text != "withKeys" { continue }
+                guard let withKeysValue = arg.expression.as(BooleanLiteralExprSyntax.self) else {
+                    throw StringSubscriptableMacroError.withKeysInvalidType
+                }
+                if withKeysValue.literal.text == "true" { generateKeys = true }
+            }
+        }
+
+        guard (declaration.is(StructDeclSyntax.self) || declaration.is(ClassDeclSyntax.self)),
+              let typeName = type.as(IdentifierTypeSyntax.self) else {
+            throw StringSubscriptableMacroError.invalidAppliedType
+        }
+
+        let keys = generateKeys ? Self.generateKeys(members: declaration.memberBlock.members) : ""
+
+        return [try ExtensionDeclSyntax("""
+        extension \(raw: typeName.name.text): StringSubscriptable {
+            \(keys)
+            subscript(key: String) -> Any? {
+                guard let kp = Self.keys[key] else {
+                    return nil
+                }
+                return self[keyPath: kp]
+            }
+        }
+        """)]
+    }
+
+    private static func generateKeys(members: MemberBlockItemListSyntax) -> DeclSyntax {
+        var keys: [String] = []
+        for m in members {
+            guard let decl = m.decl.as(VariableDeclSyntax.self),
+                  let binding = decl.bindings.first,
+                  let name = binding.pattern.as(IdentifierPatternSyntax.self) else {
+                continue
+            }
+            keys.append("\"\(camelCaseToSnakeCase(name.identifier.text))\": \\.\(name)")
+        }
+        let tab = "    "
+        keys = keys.map { "\(tab)\($0)" }
+
+        return """
+        private static let keys: [String: PartialKeyPath<Self>] = [
+        \(raw: keys.joined(separator: ",\n"))
+        ]
+        """
+    }
+
+    private static func camelCaseToSnakeCase(_ input: String) -> String {
+        // Create a regular expression to match the boundaries between lowercase and uppercase letters
+        let pattern = "([a-z])([A-Z])"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+
+        // Perform the replacement operation
+        let range = NSRange(location: 0, length: input.count)
+        let snakeCase = regex.stringByReplacingMatches(in: input, options: [], range: range, withTemplate: "$1_$2")
+
+        // Convert to lowercase
+        return snakeCase.lowercased()
+    }
+}
---
+++ Tests/SwiftRuleEngineMacrosTests/StringSubscriptableMacroTests.swift
@@ -0,0 +1,165 @@
+//
+//  StringSubscriptableMacroTests.swift
+//
+//
+//  Created by Santiago Alvarez on 04/07/2024.
+//
+
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+
+import SwiftRuleEngineMacros
+
+let testMacros: [String: Macro.Type] = [
+    "StringSubscriptable": StringSubscriptableMacro.self,
+]
+
+final class MhuntMacrosTests: XCTestCase {
+    func testMacroInStructWithKeys() throws {
+        assertMacroExpansion(
+            """
+            @StringSubscriptable(withKeys: true)
+            struct S {
+                let attr: Int = 123123
+                let attr2: String = "hello"
+            }
+            """,
+            expandedSource: """
+            struct S {
+                let attr: Int = 123123
+                let attr2: String = "hello"
+            }
+
+            extension S: StringSubscriptable {
+                private static let keys: [String: PartialKeyPath<Self>] = [
+                    "attr": \\.attr,
+                    "attr2": \\.attr2
+                ]
+                subscript(key: String) -> Any? {
+                    guard let kp = Self.keys[key] else {
+                        return nil
+                    }
+                    return self[keyPath: kp]
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testMacroInClassWithKeys() throws {
+        assertMacroExpansion(
+            """
+            @StringSubscriptable(withKeys: true)
+            class C {
+                let attr = 123
+            }
+            """,
+            expandedSource: """
+            class C {
+                let attr = 123
+            }
+
+            extension C: StringSubscriptable {
+                private static let keys: [String: PartialKeyPath<Self>] = [
+                    "attr": \\.attr
+                ]
+                subscript(key: String) -> Any? {
+                    guard let kp = Self.keys[key] else {
+                        return nil
+                    }
+                    return self[keyPath: kp]
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testMacroInStructWithoutKeys() throws {
+        assertMacroExpansion(
+            """
+            @StringSubscriptable
+            struct S {
+                let attr: Int = 123123
+            }
+            """,
+            expandedSource: """
+            struct S {
+                let attr: Int = 123123
+            }
+
+            extension S: StringSubscriptable {
+
+                subscript(key: String) -> Any? {
+                    guard let kp = Self.keys[key] else {
+                        return nil
+                    }
+                    return self[keyPath: kp]
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testMacroInClassWithoutKeys() throws {
+        assertMacroExpansion(
+            """
+            @StringSubscriptable
+            class C {
+                let attr = 123
+            }
+            """,
+            expandedSource: """
+            class C {
+                let attr = 123
+            }
+
+            extension C: StringSubscriptable {
+
+                subscript(key: String) -> Any? {
+                    guard let kp = Self.keys[key] else {
+                        return nil
+                    }
+                    return self[keyPath: kp]
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+
+    func testMacroSnakeCaseConversion() throws {
+        assertMacroExpansion(
+            """
+            @StringSubscriptable(withKeys: true)
+            class C {
+                let firstName = "Martin"
+            }
+            """,
+            expandedSource: """
+            class C {
+                let firstName = "Martin"
+            }
+
+            extension C: StringSubscriptable {
+                private static let keys: [String: PartialKeyPath<Self>] = [
+                    "first_name": \\.firstName
+                ]
+                subscript(key: String) -> Any? {
+                    guard let kp = Self.keys[key] else {
+                        return nil
+                    }
+                    return self[keyPath: kp]
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+}
