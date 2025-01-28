fbe0ad81a4cbb5b2e4b7b1499536405e0bc009c6
Shahid Rasheed <shrkhan.c@stc.com.sa>
2025-01-28 13:07:39 +0300

    Optimize rule lookups with dictionary and add duplicate name validation

--- Sources/SwiftRuleEngine/RuleEngine.swift
+++ Sources/SwiftRuleEngine/RuleEngine.swift
@@ -12,29 +12,44 @@ enum RuleEngineError: Error {
     case operatorNotFound
     case duplicateOperator
     case invalidRule(String)
+    case duplicateRuleName(String)
 }
 
 final public class RuleEngine {
+    private var rulesDict: [String: Rule] = [:]
     private var rules: [Rule] = []
     private let ruleDecoder: RuleDecoder
 
 
     public init(rules: [String], customOperators: [Operator.Type] = []) throws {
         self.ruleDecoder = try RuleDecoder(customOperators)
-        self.rules = rules.compactMap{ dictRule in
+        let decodedRules = rules.compactMap{ dictRule in
             try? decodeRule(rule: dictRule)
         }
+        try initializeRules(decodedRules)
     }
 
     public init(rules: [[String: Any]], customOperators: [Operator.Type] = []) throws {
         self.ruleDecoder = try RuleDecoder(customOperators)
-        self.rules = rules.compactMap{ dictRule in
+        let decodedRules = rules.compactMap{ dictRule in
             try? decodeRule(rule: dictRule)
         }
+        try initializeRules(decodedRules)
     }
 
     public init(rules: [Rule], customOperators: [Operator.Type] = []) throws {
         self.ruleDecoder = try RuleDecoder(customOperators)
+        try initializeRules(rules)
+    }
+    
+    private func initializeRules(_ rules: [Rule]) throws {
+        var ruleNamesSet = Set<String>()
+        self.rulesDict = try rules.reduce(into: [:]) { dict, rule in
+            if !ruleNamesSet.insert(rule.name).inserted {
+                throw RuleEngineError.duplicateRuleName("Duplicate rule name found: '\(rule.name)'")
+            }
+            dict[rule.name] = rule
+        }
         self.rules = rules
     }
 
@@ -75,7 +90,7 @@ final public class RuleEngine {
     }
     
     public func evaluateRule(named ruleName: String, with object: Any) -> Rule? {
-        guard var rule = self.rules.first(where: { $0.name == ruleName }) else {
+        guard var rule = rulesDict[ruleName] else {
             return nil
         }
         guard ((try? rule.conditions.evaluate(object)) != nil) else {
